7c1610edd13c85878f99efc87b7d8c2cbd02ecb6
erikasantanaa <erikamariasantanaa@gmail.com>
2026-03-08 02:03:05 -0300

    feat: automação login

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,6 +1,52 @@
-describe('template spec', () => {
+describe('Deve logar com sucesso', () => {
   it('passes', () => {
     cy.viewport(1440, 900)
     cy.visit('http://localhost:3000')
+
+    cy.get('#email').type('papito@webdojo.com')
+    cy.get('#password').type('katana123')
+
+    cy.contains('button', 'Entrar').click()
+    
+    cy.get('[data-cy="user-name"]')
+      .should('be.visible')
+      .and('have.text', 'Fernando Papito')
+
+    cy.get('[data-cy="welcome-message"]')
+      .should('be.visible')
+      .and('have.text','Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
+
+  })
+})
+
+describe('Não deve logar com senha inválida', () => {
+  it('passes', () => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+
+    cy.get('#email').type('papito@webdojo.com')
+    cy.get('#password').type('katana321')
+
+    cy.contains('button', 'Entrar').click()
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+
+  })
+})
+
+describe('Não deve logar com email não cadastrado', () => {
+  it('passes', () => {
+    cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+
+    cy.get('#email').type('404@webdojo.com')
+    cy.get('#password').type('katana123')
+
+    cy.contains('button', 'Entrar').click()
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+
   })
 })
