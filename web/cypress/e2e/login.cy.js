139ba79dd7e3d9bf56d39d471a57d5cb8a7f8b07
erikasantanaa <erikamariasantanaa@gmail.com>
2026-03-16 20:41:23 -0300

    feat: validação de login e acesso

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,13 +1,8 @@
 describe('Deve logar com sucesso', () => {
   it('passes', () => {
-    cy.viewport(1440, 900)
-    cy.visit('http://localhost:3000')
+    cy.startTest()
+    cy.submitLoginForm('papito@webdojo.com', 'katana123')
 
-    cy.get('#email').type('papito@webdojo.com')
-    cy.get('#password').type('katana123')
-
-    cy.contains('button', 'Entrar').click()
-    
     cy.get('[data-cy="user-name"]')
       .should('be.visible')
       .and('have.text', 'Fernando Papito')
@@ -21,13 +16,8 @@ describe('Deve logar com sucesso', () => {
 
 describe('Não deve logar com senha inválida', () => {
   it('passes', () => {
-    cy.viewport(1440, 900)
-    cy.visit('http://localhost:3000')
-
-    cy.get('#email').type('papito@webdojo.com')
-    cy.get('#password').type('katana321')
-
-    cy.contains('button', 'Entrar').click()
+    cy.startTest()
+    cy.submitLoginForm('papito@webdojo.com', 'katana321')
 
     cy.contains('Acesso negado! Tente novamente.')
       .should('be.visible')
@@ -37,13 +27,8 @@ describe('Não deve logar com senha inválida', () => {
 
 describe('Não deve logar com email não cadastrado', () => {
   it('passes', () => {
-    cy.viewport(1440, 900)
-    cy.visit('http://localhost:3000')
-
-    cy.get('#email').type('404@webdojo.com')
-    cy.get('#password').type('katana123')
-
-    cy.contains('button', 'Entrar').click()
+    cy.startTest()
+    cy.submitLoginForm('404@webdojo.com', 'katana123')
 
     cy.contains('Acesso negado! Tente novamente.')
       .should('be.visible')
---
+++ web/cypress/support/commands.js
@@ -0,0 +1,11 @@
+Cypress.Commands.add('startTest', () => {
+   cy.viewport(1440, 900)
+    cy.visit('http://localhost:3000')
+})
+
+Cypress.Commands.add('submitLoginForm', (email, senha) => {
+    cy.get('#email').type(email)
+    cy.get('#password').type(senha)
+
+    cy.contains('button', 'Entrar').click()
+})
